9fe609dcef741163a3d67a6a659c8eb0b9b90cdc
Chris Eadie <chris.eadie@outlook.com>
2020-01-27 15:59:04 +0000

    Chapter 5 - Has defulat balance and can top up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,10 @@
+require 'oystercard'
+
 describe Oystercard do
+  it "has a default balance of zero" do
+    expect(subject.balance).to eq 0
+  end
+  it 'can #top_up the balance' do
+    expect{ subject.top_up(5) }.to change{ subject.balance }.by 5
+  end
 end
